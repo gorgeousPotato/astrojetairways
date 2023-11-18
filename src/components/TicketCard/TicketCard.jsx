c63f8eccf8e11ff1280bf6412d2b22e8762adbdd
gorgeousPotato <lanauzbekov@gmail.com>
2023-11-18 00:09:17 -0800

    tickets style

--- src/components/TicketCard/TicketCard.jsx
+++ src/components/TicketCard/TicketCard.jsx
@@ -17,20 +17,38 @@ export default function TicketCard({ticket, flight}) {
   }
   return (
     <div className="TicketCard">
-      <p>PASSENGER</p>
-      <h3>{ticket.firstName.toUpperCase()} {ticket.lastName.toUpperCase()}</h3>
-      <p>ORIGIN</p>
-      <h3>{flight.spaceportD}</h3>
-      <h5>{flight.departure.toUpperCase()}</h5>
-      <p>FLIGHT</p>
-      <h3>{flight.flightN}</h3>
-      <p>DESTINATION</p>
-      <h3>{flight.spaceportA}</h3>
-      <h5>{flight.arrival.toUpperCase()}</h5>
-      <p>DATE</p>
-      <h3>{formatDate(flight.depDate)}</h3>
-      <p>TIME</p>
-      <h3>{formatTime(flight.depDate)}</h3>
+      <div className="grid-container">
+        <div className="flex-col pass-name">
+          <p>PASSENGER</p>
+          <h3>{ticket.firstName.toUpperCase()} {ticket.lastName.toUpperCase()}</h3>
+        </div>
+        <div className="flex-col">
+          <p>ORIGIN</p>
+          <h3>{flight.spaceportD}</h3>
+          <h5>{flight.departure.toUpperCase()}</h5>
+        </div>
+        <div className="flex-col">
+          <p>FLIGHT</p>
+          <h3>{flight.flightN}</h3>
+        </div>
+        <div className="flex-col">
+          <p>DESTINATION</p>
+          <h3>{flight.spaceportA}</h3>
+          <h5>{flight.arrival.toUpperCase()}</h5>
+        </div>
+        <div className="flex-col">
+          <p>DATE</p>
+          <h3>{formatDate(flight.depDate)}</h3>
+        </div>
+        <div className="flex-col">
+          <p>TIME</p>
+          <h3>{formatTime(flight.depDate)}</h3>
+        </div>
+        <div className="flex-col">
+          <p>GATE</p>
+          <h3>{flight.gate}</h3>
+        </div>
+      </div>
     </div>
   );
 }
