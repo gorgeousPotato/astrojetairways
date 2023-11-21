067c8e2f24e1ae1a7f32817a439fd6d8ccb26028
gorgeousPotato <lanauzbekov@gmail.com>
2023-11-20 23:58:11 -0800

    styles

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,14 +9,14 @@ export default function NavBar({ user, setUser }) {
   }
   return(
     
-  <nav className="NavBar navbar navbar-expand-lg bg-black rounded " aria-label="Thirteenth navbar example">
+  <nav className="NavBar navbar navbar-expand-lg rounded " aria-label="Thirteenth navbar example">
       <div class="container-fluid">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample11" aria-controls="navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse d-lg-flex" id="navbarsExample11">
-          <a class="navbar-brand col-lg-3 me-0 text-white" href="#">AstroJet</a>
+          <a class="navbar-brand col-lg-3 me-0 text-white logo" href="/flights">AstroJet</a>
           <ul class="navbar-nav col-lg-6 justify-content-lg-center">
             <li><Link to="#" class="nav-link px-2  text-light">Buy a Ticket</Link></li>
             <li><Link to="/offers" class="nav-link px-2  text-light">Special Offers</Link></li>
