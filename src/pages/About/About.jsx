26ed9303d956ff0c9d7fcbbf3acb2fbb505cffc0
gorgeousPotato <lanauzbekov@gmail.com>
2023-11-28 23:43:40 -0800

    some style

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,19 +4,15 @@ export default function About() {
   return (
     <div className="About">
       <h1>AstroJet - A Celestial Odyssey Unleashed</h1>
-      <p>In the present era, the marvels of interplanetary exploration have become a tangible reality with the existence of AstroJet, the pioneering space airline that has successfully turned cosmic dreams into extraordinary travel experiences. Launched by visionary entrepreneurs with an insatiable passion for space exploration, AstroJet stands as a testament to the human spirit of adventure and the limitless possibilities that lie beyond our home planet.
-
-The AstroJet saga commenced with the visionary leadership of Captain Stella Nova, an accomplished astronaut whose dream was to make the wonders of the universe accessible to people from all walks of life. Joined by a team of brilliant engineers, celestial scientists, and avant-garde designers, Captain Nova's vision soared to new heights, culminating in the birth of AstroJet.
-
-AstroJet's fleet, aptly named StarCruisers, epitomizes a perfect blend of state-of-the-art technology and futuristic opulence. Featuring expansive panoramic windows that provide awe-inspiring views of the cosmos, each seat is equipped with a personal stargazing device, ensuring passengers can identify constellations, planets, and distant galaxies during their celestial journey.
-
-The airline's commitment to safety and opulence extends to its expert crew—a diverse assembly of space enthusiasts, former astronauts, and consummate professionals who share an unbridled love for exploration. AstroJet is not merely a mode of transportation; it is a transformative experience that whisks passengers to the very fringes of the universe.
-
-Renowned for its exceptional service, AstroJet offers travelers more than just a vacation—it is an odyssey into the unknown. From the Martian Expedition to the Galactic Soiree, each AstroJet flight promises a unique adventure, complete with spacewalking excursions, zero-gravity play zones, and a culinary journey that mirrors the celestial landscapes.
-
-AstroJet's mission, encapsulated in its motto "A Celestial Odyssey Unleashed," symbolizes the airline's commitment to going beyond Earthly confines, expanding horizons, and uniting the global community in the shared marvel of cosmic exploration.
-
-As AstroJet continues to inspire a new era of space enthusiasts, its narrative unfolds as a testament to the spirit of innovation, passion, and the unwavering belief that the universe is a vast playground meant to be explored by all. So, fasten your seatbelts, fellow Earthlings, because with AstroJet, the celestial odyssey has already begun—an immersive journey into the cosmos that promises to be unlike any other.</p>
+      <div className="about-card">
+        <p>In the present era, the marvels of interplanetary exploration have become a tangible reality with the existence of AstroJet, the pioneering space airline that has successfully turned cosmic dreams into extraordinary travel experiences. Launched by visionary entrepreneurs with an insatiable passion for space exploration, AstroJet stands as a testament to the human spirit of adventure and the limitless possibilities that lie beyond our home planet. </p>
+        <p>The AstroJet saga commenced with the visionary leadership of Chewbacca, an accomplished astronaut whose dream was to make the wonders of the universe accessible to people from all walks of life. Joined by a team of brilliant engineers, celestial scientists, and avant-garde designers, Chewbacca's vision soared to new heights, culminating in the birth of AstroJet.</p>
+        <p>AstroJet's fleet, aptly named StarCruisers, epitomizes a perfect blend of state-of-the-art technology and futuristic opulence. Featuring expansive panoramic windows that provide awe-inspiring views of the cosmos, each seat is equipped with a personal stargazing device, ensuring passengers can identify constellations, planets, and distant galaxies during their celestial journey.</p>
+        <p>The airline's commitment to safety and opulence extends to its expert crew—a diverse assembly of space enthusiasts, former astronauts, and consummate professionals who share an unbridled love for exploration. AstroJet is not merely a mode of transportation; it is a transformative experience that whisks passengers to the very fringes of the universe.</p>
+        <p>Renowned for its exceptional service, AstroJet offers travelers more than just a vacation—it is an odyssey into the unknown. From the Martian Expedition to the Galactic Soiree, each AstroJet flight promises a unique adventure, complete with spacewalking excursions, zero-gravity play zones, and a culinary journey that mirrors the celestial landscapes.</p>
+        <p>AstroJet's mission, encapsulated in its motto "A Celestial Odyssey Unleashed," symbolizes the airline's commitment to going beyond Earthly confines, expanding horizons, and uniting the global community in the shared marvel of cosmic exploration.</p>
+        <p>As AstroJet continues to inspire a new era of space enthusiasts, its narrative unfolds as a testament to the spirit of innovation, passion, and the unwavering belief that the universe is a vast playground meant to be explored by all. So, fasten your seatbelts, fellow Earthlings, because with AstroJet, the celestial odyssey has already begun—an immersive journey into the cosmos that promises to be unlike any other.</p>
+      </div>
     </div>
   );
 }
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,3 +1,4 @@
+import { useSpring, animated } from '@react-spring/web';
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import { useState } from "react";
@@ -5,13 +6,21 @@ import "./AuthPage.css";
 
 
 export default function AuthPage( { setUser }) {
-  const [formChoice, setFormChoice] = useState('signup')
+  const [formChoice, setFormChoice] = useState('signup');
   function handleUiChoice() {
     if (formChoice === 'signup') setFormChoice('login');
     if (formChoice === 'login') setFormChoice('signup');
-  }
+  };
+  const logoAnimation = useSpring({
+    from: { opacity: 0, transform: 'scale(0.5)' },
+    to: { opacity: 1, transform: 'scale(1)' },
+    config: { duration: 1000 },
+  });
   return (
     <main className="AuthPage">
+      <animated.div style={logoAnimation}> 
+        <h1>AstroJet</h1>
+      </animated.div>
       <div className="login-signup">
         {formChoice==='signup' ? <SignUpForm setUser={ setUser }/> : <LoginForm setUser={ setUser }/>}
         <div className="p-btn">
